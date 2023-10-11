366b54ed7d4c8cb0c6e8ed7fbb25fedc960ab811
Chris Fregly <dev@chrisfregly.com>
2023-10-10 20:11:45 -0700

    cleanup

--- src/components/game/TileViewer.jsx
+++ src/components/game/TileViewer.jsx
@@ -8,67 +8,16 @@ import { BOTTOMA, BOTTOMB, LEFTA, LEFTB, RIGHTA } from "./models/farmside";
 import { FARMER } from "./models/token"
 import { LEFT, TOP } from "./models/side";
 
-// const tile = {
-//     Sides: {
-//         Top: {
-//             Structure: CITY,
-//             Colors: [RED, BLUE],
-//         },
-//         Right: {
-//             Structure: ROAD,
-//             Colors: [RED, BLUE],
-//         },
-//         Bottom: {
-//             Structure: FARM,
-//             Colors: [],
-//         },
-//         Left: {
-//             Structure: ROAD,
-//             Colors: [RED],
-//         },
-//         Center: {
-//             Structure: NIL,
-//             Colors: [],
-//         },
-
-//         BottomA: {
-//             Structure: FARM,
-//             Colors: [RED, BLUE],
-//         }, 
-//         // BottomB: {
-//         //     Structure: FARM,
-//         //     Colors: [RED],
-//         // },
-//         // RightA: {
-//         //     Structure: FARM,
-//         //     Colors: [RED],
-//         // },
-//         LeftB: {
-//             Structure: FARM,
-//             Colors: [RED],
-//         }
-//         // TODO merge tile.FarmTeams into tile.Teams into tile.CenterTeam using tile.Sides
-//     },
-//     CityConn: false,
-//     Banner: false
-// }
-
-// const token = {
-//     Side: BOTTOMA,
-//     Color: BLUE
-// }
-
 const tile = {
     Sides: { Top: { Structure: ROAD, Colors: [RED] }, Right: { Structure: ROAD, Colors: [] }, Bottom: { Structure: ROAD, Colors: [] }, Left: { Structure: ROAD, Colors: [] }, Center: { Structure: NIL, Colors: [] }, RightA: { Structure: FARM, Colors: [RED] }},
     CityConn: false,
     Banner: false
 }
-
 const token = null
-
 const tokenDroppable = false
 
-export const Test = () => {
+// for testing purposes only
+export const TileViewer = () => {
 
     const sensors = useSensors(
         useSensor(PointerSensor, {
